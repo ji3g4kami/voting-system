24ceffae5a5ec3774e619f6f3784a7399d8f8ad3
Deng Li <ji3g4kami@gmail.com>
2017-10-30 23:29:57 +0800

    basic voting system

---
+++ app/controllers/candidates_controller.rb
@@ -0,0 +1,43 @@
+class CandidatesController < ApplicationController
+  def index
+    @candidates = Candidate.all
+  end
+
+  def new
+    @candidate = Candidate.new
+  end
+
+  def create
+    @candidate = Candidate.new(candidate_params)
+    if @candidate.save
+      redirect_to candidates_path, notice: "新增候選人成功！"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @candidate = Candidate.find(params[:id])
+  end
+
+  def update
+    @candidate = Candidate.find(params[:id])
+    if @candidate.update(candidate_params)
+      redirect_to candidates_path, notice: "修改候選人成功！"
+    else
+      render :edit
+    end
+  end
+
+  def vote
+    @candidate = Candidate.find(params[:id])
+    @candidate.increment(:votes)
+    @candidate.save
+    redirect_to candidates_path, notice: "完成投票！"
+  end
+
+  private
+    def candidate_params
+      params.require(:candidate).permit(:name, :age, :party, :politics)
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :candidates do
+    member do
+      post :vote
+    end
+  end
+  root "candidates#index"
+end
